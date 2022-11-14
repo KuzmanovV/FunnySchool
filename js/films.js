f3dfd7ca97efa0cc92969f6c59aad0c51d6e5653
Victor Kuzmanov <75267166+KuzmanovV@users.noreply.github.com>
2022-11-14 15:38:07 +0200

    snapshot to movie added

--- js/films.js
+++ js/films.js
@@ -4,6 +4,7 @@ import renderFunction from './moduls/renderFunction.js';
 const filmTemplate = (data) => html`
 <div class="film">
         <a href=${data.uLink}>${data.title}</a>
+        <img class="snap" src=${data.filmImg} alt="a snapshot from the movie" />
       </div>`;
 
 renderFunction('.films', '../data/films.json', filmTemplate);
