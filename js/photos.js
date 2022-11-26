5d8a9e8a820eeb5c3ac1ae67f4a88bd482722a47
Victor Kuzmanov <victorkuzmanov@gmail.com>
2022-11-26 09:40:05 +0200

    Added photos with bootstrap, jquery & ekkolit

--- js/photos.js
+++ js/photos.js
@@ -1,16 +1,10 @@
 import {html} from '../node_modules/lit-html/lit-html.js';
 import renderFunction from './moduls/renderFunction.js';
 
-const songTemplate = (data) => html`
-<div class="song">
-        <label>${data.title}</label>
-        <audio controls>
-          <source
-            src=${data.song}
-            type=${data.type}
-          />
-        </audio>
-      </div>`;
+const filmTemplate = (data) => html`
+<a href=${data.photo} data-toggle="lightbox" data-gallery="example-gallery" class="col-xl-6 col-md-4 box-1">
+                            <img src=${data.photo} class="img-fluid">
+                                <p class="photoName">${data.title}</p>
+                          </a>`;
 
-// renderFunction('.songs', '../data/songs.json', songTemplate);
-
+renderFunction('.row', '../data/photos.json', filmTemplate);
