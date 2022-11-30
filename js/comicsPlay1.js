05bfdd7a8cb8a1736e15de5aedca42b2e85e39ee
Victor Kuzmanov <victorkuzmanov@gmail.com>
2022-11-30 22:49:48 +0200

    All headers with lit

--- js/comicsPlay1.js
+++ js/comicsPlay1.js
@@ -1,11 +1,15 @@
 import {html, render} from '../node_modules/lit-html/lit-html.js';
-import {renderMultimedia, footerTemplate} from './moduls/renderFunctions.js';
+import {renderMultimedia, footerTemplate, headerTemplate} from './moduls/renderFunctions.js';
+import toggleMenu from './moduls/headerAnim.js';
+
+toggleMenu();
 
 const comicsPlayTemplate = (data) => html`
 <a href=${data.page} data-toggle="lightbox" data-gallery="example-gallery" class="col-xl-6 col-md-4 box-1">
                             <img src=${data.page} class="img-fluid">
                           </a>`;
 
+render(headerTemplate('DAGA - 1'), document.querySelector('header'));
 renderMultimedia('.row', '../data/comicsPlay1.json', comicsPlayTemplate);
 render(footerTemplate, document.body);
 
